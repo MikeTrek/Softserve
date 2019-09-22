9decbfcba8e82f7b6d45efcbfaed8c0396e991ea
myevs <myevs@sofrserveinc.com>
2019-09-22 13:54:04 +0300

    new

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,15 @@
 package com.company;
 
+import javafx.scene.shape.HLineTo;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
         test line test 2
+            test
+  new HLineTo( )
+
+          test
     }
 }
